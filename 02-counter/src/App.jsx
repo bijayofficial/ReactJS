9ae191372396baca58e2e80930b5d929cc6391d1
Bijay Ghosh <bijayghosh.email.com>
2025-03-09 11:33:12 +0530

    counter react second commit

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 import './App.css'
 
 function App() {
   
+  
+  const [counter , setCounter] = useState(0);
+
+  function addValue() {
+    setCounter(counter + 5);
+  }
+  function removeValue() {
+    setCounter(counter - 5);
+  }
   return (
     <>
       
-      
       <h1>chai or react</h1>
-      <h2>Counter Value : 5</h2>
-      <button type="button">ADD VALUE</button>
+      <h2>Current Value : {counter}</h2>
+      <button type="button" 
+      onClick={addValue}  // reference of function: will run after the btn click
+      >ADD VALUE</button>
       
-      <button type="button"> REMOVE VALUE </button>
+      <button type="button"
+      onClick={removeValue}
+      > REMOVE VALUE </button>
 
     </>
   )
