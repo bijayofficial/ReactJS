3671646af7048f993f7fe754504691e14c14d3cd
Bijay Ghosh <bijayghosh.email.com>
2025-03-09 09:41:56 +0530

    counter react

---
+++ 02-counter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      
+      
+      <h1>chai or react</h1>
+      <h2>Counter Value : 5</h2>
+      <button type="button">ADD VALUE</button>
+      
+      <button type="button"> REMOVE VALUE </button>
+
+    </>
+  )
+}
+
+export default App
