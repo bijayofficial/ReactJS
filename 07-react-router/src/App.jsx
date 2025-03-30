4017984175efaf975cc72c5ea3c657bad4960f94
Bijay Ghosh <bijayghosh.email.com>
2025-03-30 10:27:35 +0530

    React Router Dom

---
+++ 07-react-router/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
+function App() {
+ 
+   
+  return (
+    <>
+    <Header/>
+    
+    Hello
+    
+
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App;
